5d61375e74531dcf5f1ce2d4b183824a295639b1
Ivan Viso <ivisoaltamirano@danielcastelao.org>
2020-11-10 17:25:30 +0100

    s

--- Noviembre/14.sh
+++ Noviembre/14.sh
@@ -1,20 +1,31 @@
-PS3='Please enter your choice: '
-options=("Mostrar fecha" "Mostrar usuarios conectados" "Mostrar el directorio de trabajo" "" "Quit")
-select opt in "${options[@]}"
+PS3="Elija su opcion: "
+opciones=("Mostrar fecha" "Mostrar usuarios conectados" "Mostrar el directorio de trabajo" "Listar el contenido del directorio de trabajo")
+select opt in "${opciones[@]}" #creamos un array, y seleccionamos valores segun su contenido, asi, su indice nos sirve para generar un menu
 do
     case $opt in
-        "Option 1")
-            echo "you chose choice 1"
-            ;;
-        "Option 2")
-            echo "you chose choice 2"
-            ;;
-        "Option 3")
-            echo "you chose choice $REPLY which is $opt"
-            ;;
-        "Quit")
+        "Mostrar fecha")
+            clear
+            echo "la fecha es : "
+            date
             break
-            ;;
-        *) echo "invalid option $REPLY";;
+        ;;
+        "Mostrar usuarios conectados")
+            clear
+            echo "los usuarios conectados son"
+            users
+            break
+        ;;
+        "Mostrar el directorio de trabajo")
+            clear
+            echo "el directorio de trabajo es: " $PWD
+            break
+        ;;
+        "Listar el contenido del directorio de trabajo")
+            clear
+            echo "el contenido del directorio de trabajo :"
+            ls -l
+            break
+        ;;
+        *) echo "Opcion no valida";;
     esac
 done
